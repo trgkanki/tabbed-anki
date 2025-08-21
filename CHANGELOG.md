79a1bc42a71cef9177fa8028b2ced99c5f7f0ffc
Park Hyunwoo <phu54321@naver.com>
2025-08-22 08:57:50 +0900

    version: v25.8.21i255
    - Initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog of tabbed
+
+[comment]: # (DO NOT MODIFY. new changelog goes here)
+
+## 25.8.21i255 (2025-08-22)
+
+- Initial release
--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@
 
 # -*- coding: utf-8 -*-
 #
-# tabbed-anki v20.5.4i8
+# tabbed v25.8.21i255
 #
 # Copyright: trgk (phu54321@naver.com)
 # License: GNU AGPL, version 3 or later;
